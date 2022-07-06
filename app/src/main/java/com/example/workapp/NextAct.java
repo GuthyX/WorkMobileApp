d0274f056b3a3aa00517377ea7db5dabb43c3b6b
Guti Adrián <g.a.a612.ag@gmail.com>
2022-07-06 17:38:21 +0200

    Splash screen implement

---
+++ app/src/main/java/com/example/workapp/NextAct.java
@@ -0,0 +1,14 @@
+package com.example.workapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
